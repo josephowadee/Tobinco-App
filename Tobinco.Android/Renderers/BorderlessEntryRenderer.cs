45fc6230bef6fe0cf97e05f4e278d6be23b660ee
Joseph Owadee <65883659+josephowadee@users.noreply.github.com>
2022-04-08 22:49:56 -0400

    Add files via upload

---
+++ Tobinco.Android/Renderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.Content;
+using Tobinco.Droid.Renderers;
+using Tobinco.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace Tobinco.Droid.Renderers
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+        public BorderlessEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
