136a173c1329723f466a8d0037c138e9e9376771
Joseph Owadee <65883659+josephowadee@users.noreply.github.com>
2022-04-08 22:41:07 -0400

    Add files via upload

---
+++ Tobinco.iOS/Renderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Tobinco.iOS.Renderers;
+using Tobinco.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace Tobinco.iOS.Renderers
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (Control == null) return;
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+
+        }
+    }
+}
